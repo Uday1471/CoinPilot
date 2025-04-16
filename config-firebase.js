7e6ce852f2ef587225efd8c4a80bdecd61d0bb84
Tushar1458 <tkumar2808@gmail.com>
2025-04-16 14:52:13 +0530

    Added auth.js and config-firebase.js and dashboard.html

---
+++ config-firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCNXuQDGoCdxCqhn9fP7HPo-kd_BVyJJGc",
+  authDomain: "personal-finance-tracker-b96bf.firebaseapp.com",
+  projectId: "personal-finance-tracker-b96bf",
+  storageBucket: "personal-finance-tracker-b96bf.firebasestorage.app",
+  messagingSenderId: "229773200863",
+  appId: "1:229773200863:web:3337382d85d7250b6d886a",
+  measurementId: "G-MWPE179L4D",
+};
+
+// Initializing Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
